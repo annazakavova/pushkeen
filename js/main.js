e3b294bf043b343e63863d4f888657d7adb181a5
AnnaZakavova <zakavova94@icloud.com>
2022-03-28 21:11:22 +0300

    removing unnecessary library

--- js/main.js
+++ js/main.js
@@ -25,18 +25,39 @@
 
 
 SWIPER
- var Swiper = new Swiper ('.image-slider', {
-	navigator: {
-		nextEl: ".swiper-button-next",
-    	prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		type: 'fraction',
-		renderFraction: function (currentClass, totalClass) {
-			return 'photo <span class="' + currentClass + '"></span>' +
-			' из ' + '<span class="' + totalClass + '"></span>';
-		},
-	},
+//  var Swiper = new Swiper ('.image-slider', {
+// 	navigator: {
+// 		nextEl: ".swiper-button-next",
+//     	prevEl: ".swiper-button-prev",
+// 	},
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 		type: 'fraction',
+// 		renderFraction: function (currentClass, totalClass) {
+// 			return 'photo <span class="' + currentClass + '"></span>' +
+// 			' из ' + '<span class="' + totalClass + '"></span>';
+// 		},
+// 	},
 	
+//  });
+
+// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+	interval: false
+ });
+ 
+ // for every slide in carousel, copy the next slide's item in the slide.
+ // Do the same for the next, next item.
+ $('.multi-item-carousel .item').each(function(){
+	var next = $(this).next();
+	if (!next.length) {
+	  next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+	
+	if (next.next().length>0) {
+	  next.next().children(':first-child').clone().appendTo($(this));
+	} else {
+		$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+	}
  });
